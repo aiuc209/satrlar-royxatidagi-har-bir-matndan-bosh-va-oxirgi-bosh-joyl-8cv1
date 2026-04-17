6c88ae828b6efd4704295141696e0363e68e4dd1
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 00:37:37 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Satrlar ro‘yxatidagi har bir matndan bosh va oxirgi bo‘sh joylarni olib tashlang (strip).")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
